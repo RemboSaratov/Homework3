ea9145862df820fcbe757e3c3aff1c0fcdeb20c9
Keenan88 <rembosaratov@gmail.com>
2023-12-25 16:06:44 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int table = 1;
+        byte myMoney = -100;
+        short hands = 2;
+        long billGatesMoney = 1000000000L;
+        System.out.println("Значение переменной " + "table " + "с типом " + "int "  + "равно " + table  );
+        System.out.println("Значение переменной " + "myMoney " + "с типом " + "byte "  + "равно " + myMoney);
+        System.out.println("Значение переменной " + "hands " + "с типом " + "short "  + "равно " + hands  );
+        System.out.println("Значение переменной " + "billGatesMoney " + "с типом " + "long "  + "равно " + billGatesMoney  );
+
+
+
+
+    }
+}
